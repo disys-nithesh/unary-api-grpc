670572c1d9208f052e611104bb08859cd2e83481
disys-nithesh <95427043+disys-nithesh@users.noreply.github.com>
2022-04-04 18:31:11 +0530

    Add files via upload

---
+++ greeting_unary_api/src/main/java/com/grpc/unary/greeting_unary_api/GreetingUnaryApiApplication.java
@@ -0,0 +1,13 @@
+package com.grpc.unary.greeting_unary_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreetingUnaryApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreetingUnaryApiApplication.class, args);
+	}
+
+}
---
+++ greeting_unary_api/src/test/java/com/grpc/unary/greeting_unary_api/GreetingUnaryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grpc.unary.greeting_unary_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreetingUnaryApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
